723f348ed41907df7954a903ebbd7541cb545d0b
Chulki Lee <chulki.lee@gmail.com>
2018-03-28 16:50:23 -0700

    initial commit

---
+++ lib/metadata_logger_json_formatter.ex
@@ -0,0 +1,18 @@
+defmodule MetadataLoggerJsonFormatter do
+  @moduledoc """
+  Documentation for MetadataLoggerJsonFormatter.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MetadataLoggerJsonFormatter.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/metadata_logger_json_formatter_test.exs
@@ -0,0 +1,8 @@
+defmodule MetadataLoggerJsonFormatterTest do
+  use ExUnit.Case
+  doctest MetadataLoggerJsonFormatter
+
+  test "greets the world" do
+    assert MetadataLoggerJsonFormatter.hello() == :world
+  end
+end
