92cc4a73166f92af57a60bdde7b163c8aa960a00
Chulki Lee <chulki.lee@gmail.com>
2018-08-09 10:14:00 -0700

    add hex pkg info and CHANGELOG.md

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule MetadataLoggerJsonFormatter.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # hex
+      description: "Logger formatter to print message and metadata in a single-line json",
+      package: package()
     ]
   end
 
@@ -20,6 +24,18 @@ defmodule MetadataLoggerJsonFormatter.MixProject do
   defp deps do
     [
       {:jason, "~> 1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/chulkilee/metadata_logger_json_formatter",
+        "Changelog" =>
+          "https://github.com/chulkilee/metadata_logger_json_formatter/blob/master/CHANGELOG.md"
+      },
+      maintainers: ["Chulki Lee"]
     ]
   end
 end
