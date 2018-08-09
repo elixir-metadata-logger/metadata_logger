0a8085ba84eaa4048f16523172ed81e507ef5b1e
Chulki Lee <chulki.lee@gmail.com>
2018-08-09 10:34:31 -0700

    Bump to v0.1.1-dev

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 # Changelog
 
 ## [Unreleased]
+
+## 0.1.0 - 2018-08-08
+
+[Unreleased]: https://github.com/chulkilee/metadata_logger_json_formatter/compare/v0.1.0...HEAD
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MetadataLoggerJsonFormatter.MixProject do
   def project do
     [
       app: :metadata_logger_json_formatter,
-      version: "0.1.0",
+      version: "0.1.1-dev",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
