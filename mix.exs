a3f1c49271323b32dae4ba179fc97ff673effab4
Chulki Lee <chulki.lee@gmail.com>
2019-11-06 20:51:02 -0800

    remove config and extra_applications

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule MetadataLoggerJsonFormatter.MixProject do
 
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: []
     ]
   end
 
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,2 @@
 ExUnit.start()
+Logger.App.start()
